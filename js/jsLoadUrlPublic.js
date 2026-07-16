11b1e070c3d27020c99cb85ffa26c5b5e56d0058
amihaji <amihaji@gmail.com>
2026-07-16 09:56:25 +0800

    update
    update

--- js/jsLoadUrlPublic.js
+++ js/jsLoadUrlPublic.js
@@ -5,7 +5,7 @@
 // URl dbUSER (TabelUser, LogNotif) pada aplikasi : jsDashboard.js, jsSetupUser.js untuk loginBeratideal.html, formDashboard.html
 const URL_dbUser      = 'https://script.google.com/macros/s/AKfycbySEM1PMsIFOm-ovW0_Alte_zX7bCDYNhmZ5ySePR26P5DFnVkM0LCXUGnAAqH9k_FlqA/exec';
 // URl dbWETools (SurveyData, DataWE) pada aplikasi : jsDashboard.js, jsFollowWe.js untuk formDashboard.html
-const URL_dbWETools   = 'https://script.google.com/macros/s/AKfycbyhFOm3xckRf66I8sM5lJFhJUBeLpFHIjrRpz1Fuu1OuKyzR0W2O0ReGc6MmiW9k-dT/exec';
+const URL_dbWETools   = 'https://script.google.com/macros/s/AKfycbzUFteufKHNidJsixw0RODEBry71vg_aVCl6RstGQlKdRVRg36HiDmxTsWp9p7dvNE/exec';
 // url dbProgram (PROGRAM , DATAKONSUMEN) pada aplikasi : jsFitTracker.js, jsFollowCrm.js untuk formDashboard.html
 const URL_dbProgram   = 'https://script.google.com/macros/s/AKfycbwV2mZySfzMevxDroycyx3Nadwu6SR-Fn9MEJLZaV8AONHVO8YW9AhevaQ7p_Bwm_b1DQ/exec';
 // url dbProgram (PROGRAM , DATAKONSUMEN) untuk prog10hari.html
