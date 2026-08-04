bfcf22659db8cf971392ae67a55e6608fd8efc46
amihaji <amihaji@gmail.com>
2026-08-04 13:08:15 +0800

    Update jsDashboard.js
    update

--- js/jsDashboard.js
+++ js/jsDashboard.js
@@ -864,7 +864,7 @@ function buildPesertaFollowUpMessage(template, peserta) {
         return mainMessage;
     }
 
-    return `${mainMessage}\n\nNB : balas pesan ini ke ${sponsorName} di wa ${sponsorPhone}`;
+    return `${mainMessage}\n\n*NB: Balas pesan ini ke ${sponsorName} di WA: ${sponsorPhone}*`;
 }
 
 function showPesertaFollowUpStatus(type, message, duration = 4000) {
