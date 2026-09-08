40420328704f8b9225aa51dee4d75ac4b9df88ec
amihaji <amihaji@gmail.com>
2026-09-08 09:05:55 +0800

    Update jsProg10hari.js
    update

--- js/jsProg10hari.js
+++ js/jsProg10hari.js
@@ -7874,13 +7874,9 @@ function tampilkanDashboardSederhana(data) {
                         ${tukarPointDisabledAttr}>
                         <i class="fas fa-exchange-alt"></i> Tukar Point Sekarang
                     </button>
-                   <!-- 
-                   <button class="btn ${unduhSertifikatButtonClass} btn-sm mt-1 mb-3" 
-                        onclick="unduhSertifikat" 
-                        ${unduhSertifikatDisabledAttr}>
+                    <button class="btn btn-sm mt-1 mb-3">
                         <i class="fas fa-cloud-download-alt"></i> Download Sertifikat
                     </button>
-                    -->
                 </div>
             </div>
         </div>
