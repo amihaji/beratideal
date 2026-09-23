83e44e664a86a5b0dc9681adc6f2acc8a7f441f3
amihaji <amihaji@gmail.com>
2026-09-23 17:08:58 +0800

    Update jsLoadUrlPublic.js
    update

--- js/jsLoadUrlPublic.js
+++ js/jsLoadUrlPublic.js
@@ -32,7 +32,7 @@ const URL_dbReferral = 'https://script.google.com/macros/s/AKfycbx5W5XBlSIb_GLqC
 // url dbEstihtools (DataInput, DataKonsumen, TabelHarga, TabelDiskon, TabelByKirim, TabelKategori) :
 // jsEstihTools.js 
 // untuk estihtoolsBeratideal.html
-const URL_dbEstihtools = 'https://script.google.com/macros/s/AKfycbxdooCUkpXqER-wXfO824z_3nkKeULYB6ZnyrUaHvGrJUgrqiDkMT6i4glWh4WsW_QT/exec';
+const URL_dbEstihtools = 'https://script.google.com/macros/s/AKfycbxD9Ogo7d1BI3h6bwo8mCtN23Vr8gvSf8QYc9Ei93gd4zGtqDgHf3gXC435YmqV3xgi/exec';
 
 // ===========================
 // FUNGSI GLOBAL UNTUK TOOLTIP
